522feef671495d0a1b13afbee405d929d0a43cf0
Mr.Joker <75469718+vv1ck@users.noreply.github.com>
2023-07-19 08:30:43 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,3 +1,4 @@
+def login():
 	sisn='52645750083%3AJDdvBJDNPYSiQC%3A28%3AAYfHuv69uqMWp54x-NSj2fMNokREhcuYQZ2ZR3xn5g'
 	headers={"User-Agent": "Instagram 237.0.0.14.102 Android (28/9; 640dpi; 1440x2560; HUAWEI; LON-L29; HWLON; hi3660; ar_US; 373310554)","Content-Type": "application/x-www-form-urlencoded",'Accept-Encoding': 'gzip, deflate, br,ar',"Acceptk-Language": "ar-US","Connection": "keep-alive","Accept": "*/*","cookie": 'sessionid='+sisn,"x-csrftoken": "missing"}
 	input('user')
